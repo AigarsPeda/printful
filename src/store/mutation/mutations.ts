f6f54e1f66c1dc3e440ce680318b45cf21a8985f
Aigars <aigarspeda@gmail.com>
2023-08-25 20:14:28 +0300

    bug fix for scale change

--- src/store/mutation/mutations.ts
+++ src/store/mutation/mutations.ts
@@ -54,13 +54,13 @@ const mutations: MutationTree<StateType> & MutationsType = {
     state: StateType,
     payload: { ids: string[]; scaleX: number; scaleY: number }
   ) {
-    for (const key of state.canvas) {
-      const id = key.id as keyof StateType['canvasObject']
+    for (const savedCanvas of state.canvas) {
+      const id = savedCanvas.id as keyof StateType['canvasObject']
 
-      for (const key of state.canvasObject[id]) {
-        if (payload.ids.includes(key.id)) {
-          key.width = key.width * payload.scaleX
-          key.height = key.height * payload.scaleY
+      for (const obj of state.canvasObject[id]) {
+        if (payload.ids.includes(obj.id)) {
+          obj.width = obj.width * payload.scaleX
+          obj.height = obj.height * payload.scaleY
         }
       }
     }
